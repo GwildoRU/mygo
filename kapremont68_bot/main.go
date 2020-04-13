35dcae05e37b7f4e327ad88dd0e83a75d1438bff
GERA <gerasimenko@kapremont.ru>
2020-04-13 16:08:49 +0300

    -

--- kapremont68_bot/main.go
+++ kapremont68_bot/main.go
@@ -3,20 +3,17 @@ package main
 import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
-
-	"net/http"
-	"net/url"
 )
-
 
 //https://uk-167-116-1.friproxy0.eu:443 [UK]
 //https://ua-139-170-1.fri-gate0.biz:443 [UA]
 //https://fr-54-189-1.friproxy.eu:443 [FR]
 
 func main() {
-	proxyUrl, err := url.Parse("https://uk-167-116-1.friproxy0.eu:443")
-	myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
-	bot, err := tgbotapi.NewBotAPIWithClient("1173218587:AAFeNbm9qiJubZyMR79_VU4a3HFO9nkRrpw",tgbotapi.APIEndpoint,myClient)
+	//proxyUrl, err := url.Parse("https://uk-167-116-1.friproxy0.eu:443")
+	//myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+	//bot, err := tgbotapi.NewBotAPIWithClient("1173218587:AAFeNbm9qiJubZyMR79_VU4a3HFO9nkRrpw",tgbotapi.APIEndpoint,myClient)
+	bot, err := tgbotapi.NewBotAPI("1173218587:AAFeNbm9qiJubZyMR79_VU4a3HFO9nkRrpw")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -35,10 +32,12 @@ func main() {
 			continue
 		}
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		//log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		log.Println(update.Message)
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text+" => qqqqqqqqqqqq")
-		msg.ReplyToMessageID = update.Message.MessageID
+		//msg.ReplyToMessageID = update.Message.MessageID
 
 		bot.Send(msg)
 	}
