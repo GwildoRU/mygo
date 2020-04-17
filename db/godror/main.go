f12e6cbe493d6992541aa1b0f0a2d3ab25613ed2
GERA <gerasimenko@kapremont68.ru>
2020-04-17 16:23:27 +0300

    -

--- db/godror/main.go
+++ db/godror/main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("godror", "FCR/Qpwo1029@10.55.168.201:1521/FCR1")
+	db, err := sql.Open("godror", "FCR_READ_ONLY/readonly@10.55.168.201:1521/FCR1")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT addr FROM mv_houses_adreses where HOUSE_ID = 2361")
+	rows, err := db.Query("SELECT addr, TO_CHAR(HOUSE_ID) FROM mv_houses_adreses where HOUSE_ID = 2361")
 	if err != nil {
 		panic(err)
 	}
@@ -21,10 +21,11 @@ func main() {
 
 	for rows.Next() {
 		var s string
-		if err := rows.Scan(&s); err != nil {
+        var d int
+		if err := rows.Scan(&s, &d); err != nil {
 			panic(err)
 		}
-		fmt.Println(s)
+		fmt.Println(s, d)
 	}
 
 }
